fc1e6dd53a0339a37c391fbbb32b19f244e52777
chilledguru <chilledguru@gmail.com>
2020-03-25 02:01:08 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>MP3Juice - Free MP3 Music Downloader</title>
+<base href="https://mp3-juice.site">
+<meta name="keywords" content="mp3juice, mp3 juice site">
+<meta name="description" content="Welcome to MP3Juice site for free download music is a known free mp3 search engine for mp3 convert and download.">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@eMp3Juices">
+<meta name="twitter:title" content="MP3Juice - Free MP3 Music Downloader">
+<meta name="twitter:description" content="Welcome to MP3Juice site for free download music is a known free mp3 search engine for mp3 convert and download.">
+<meta name="twitter:creator" content="@eMp3Juices">
+<meta name="twitter:image" content="https://mp3-juice.site/favicon/apple-icon-120x120.png">
+
+<meta itemprop="name" content="MP3Juice - Free MP3 Music Downloader">
+<meta itemprop="description" content="Welcome to MP3Juice site for free download music is a known free mp3 search engine for mp3 convert and download.">
+<meta itemprop="image" content="https://mp3-juice.site/favicon/apple-icon-120x120.png">
+<meta name="robots" content="index,follow">
+<link rel="canonical" href="https://mp3-juice.site">
+<meta name="google-site-verification" content="nKMu5AccSQ0W15RNXqo5rvIyYzw_pazt-WImb6bjjWI" />
+<link rel="apple-touch-icon" sizes="57x57" href="https://mp3-juice.site/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://mp3-juice.site/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://mp3-juice.site/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://mp3-juice.site/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://mp3-juice.site/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://mp3-juice.site/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://mp3-juice.site/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://mp3-juice.site/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://mp3-juice.site/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://mp3-juice.site/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://mp3-juice.site/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://mp3-juice.site/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://mp3-juice.site/favicon/favicon-16x16.png">
+
+<meta name="msapplication-TileImage" content="https://mp3-juice.site/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#2b4c6e">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+
+
+
+
+<script data-cfasync="false" async type="text/javascript" src="//eondunpea.com/1clkn/18446"></script>
+
+<style>.name {
+                font-family:    Helvetica, sans-serif
+                font-size:      650px; color:#ffffff;
+                font-weight:    bold;
+            } body{color:#ffffff; background-color:#2b4c6e; font-size:13px; font-family:'Open Sans', sans-serif;font-weight:400;     margin:0;} #nav{max-width:600px;border-bottom:1px #156aae solid;margin:0 auto 30px;padding:5px 0}#nav a{color:#fff;text-decoration:none;margin-right:5%}#nav a:last-child{margin-right:0}#content{max-width:700px;margin:0 auto}#content #logo{border:none;margin:0 auto 20px}.formd{background-color:#fff;width:100%;height:42px;border-radius:2px;margin:0 auto 5px;position:relative}form #query{color:#2c2c2c;background-color:transparent;font-size:14px;font-family:'Open Sans',sans-serif;float:left;width:89.666666666667%;height:42px;border:none;padding:0 1%}form #button{color:#2c2c2c;background-color:transparent;font-size:21px;float:left;width:8.3333333333333%;height:42px;border:none;padding:0;cursor:pointer}#form_text{font-size:11px}#form_text a:link,#form_text a:visited{color:#fff}#form_text a:hover,#form_text a:active{text-decoration:none}#control_sources{color:#fff;background-color:#156aae;font-weight:600;text-decoration:none;border-radius:2px;margin-top:15px;padding:5px 1.6666666666667%;display:inline-block}#sources{width:38.333333333333%;margin:15px auto 0;display:none}#sources #left{float:left;width:43.478260869565%;margin-right:13.04347826087%}#sources #right{float:left;width:43.478260869565%}#sources a{color:#fff;font-weight:600;text-decoration:none;border-radius:2px;margin-bottom:15px;padding:5px 0;display:block}#sources .enabled{background-color:#2c2c2c}#sources .disabled{background-color:#156aae}#load{font-size:50px;width:100%;margin:60px auto;display:none}#text{text-align:justify;width:100%;margin-top:60px}#text h1{font-size:26px;font-weight:400;text-align:center;margin:0 0 5px}#text p{width:100%;line-height:23px}#text p:first-of-type{margin-top:0}#text p:last-of-type{margin-bottom:0}#text a:link,#text a:visited{color:#fff}#text a:hover,#text a:active{text-decoration:none}#footer{max-width:600px;border-top:1px #156aae solid;margin:30px auto 0;padding:5px 0}#footer a{color:#fff;text-decoration:none;margin-right:5%}#footer a:last-child{margin-right:0}.clear{clear:both}#error{text-align:center;width:100%;margin:60px auto}#error a{color:#fff}#results{text-align:justify;width:100%;margin:60px auto}#results p{line-height:23px;margin:0}#results p:last-of-type{margin-bottom:15px!important}#results p a{color:#fff}#results #results_sources{font-weight:600;text-align:center;margin:5px 0}.text{color:#2c2c2c;background-color:#fff;text-align:justify;width:96.666666666667%;border-radius:2px;margin-top:15px;padding:1.6666666666667%}.text h2{font-size:15px;font-weight:700;margin:0}.text p{line-height:23px;width:100%}.text p:first-of-type{margin-top:0}.text p:last-of-type{margin-bottom:0}.text strong{font-weight:700}.text img{width:100%}.text .lt{text-decoration:line-through}.text .i{font-style:italic;font-weight:400}.text .b{font-weight:700}.text ul{font-size:13px;font-family:'Open Sans',sans-serif;font-weight:400;line-height:23px;list-style-position:inside;list-style-type:lower-alpha;margin:0;padding:0}.text ul li{margin-bottom:15px}.text ul li:last-child{margin-bottom:0}.text ul li ul{width:96.551724137931%;list-style-type:lower-roman;margin-top:15px;padding:0 0 0 3.448275862069%}.text li div{text-align:center;margin:15px 0}.text a:link,.text a:visited{color:#2c2c2c}.text a:hover,.text a:active{text-decoration:none}.text #cutter{text-align:center;width:100%;border-top:1px #f0f0f0 solid;margin-top:15px;padding-top:15px}.text .date{color:#989898;font-size:11px;margin:0}.result{background-color:#156aae;text-align:center;width:96.666666666667%;border-radius:2px;margin-bottom:15px;padding:1.6666666666667%}.result .name{font-weight:600;text-overflow:ellipsis;white-space:nowrap;width:100%;overflow:hidden}.result .properties{font-size:11px;width:100%;margin:0 auto 15px}.result .options a{color:#2c2c2c;background-color:#fff;font-weight:600;text-decoration:none;border-radius:2px;margin-right:.83333333333333%;padding:5px 1.6666666666667%;display:inline-block}.result .options a:last-child{margin-right:0}#player{background-color:#fff;width:96.666666666667%;border-radius:2px;margin-bottom:15px;padding:1.6666666666667%;overflow:hidden}iframe{border:none}.ephp{margin-bottom:5px;margin-top:5px}.lr{margin-left:5px;margin-right:5px}tett{overflow:hidden;text-overflow:ellipsis;display:-webkit-box;line-height:20px;max-height:28px;-webkit-line-clamp:1;-webkit-box-orient:vertical}.emailbox{margin:15px}.shortbox{margin-bottom:2px}.alert-danger{overflow:hidden;color:#fff;background:#E60303}.alert-success{overflow:hidden;color:#fff;background:#009D0B}.artwork{float:left;padding:5px}.artwork img{border-radius:3px;width:80px}.meta{text-align:left;margin-left:20px;display:table-cell;padding-left:8px;height:67px;overflow:hidden;vertical-align:middle}.publisher{color:#A2A2A2;font-size:80%} .btn-primary { color: #ffffff; background-color: #2b4c6e; background-image: linear-gradient(to bottom, #2964ac, #2964ac);border-color: #2B669A #2B669A #2B669A;}  .btn.btn-default { color: #333333; background-color: #FFFFFF; background-image: linear-gradient(to bottom, #FFFFFF, #ffffff); border-color: #CCCCCC #CCCCCC #CCCCCC;}</style></head>
+<body><center>
+<div id="nav">
+<a href="">Home</a><a href="https://www.facebook.com/ytmp3juicecom/" target="_blank">Facebook</a><a href="https://www.instagram.com/ytmp3juice.free.mp3.download/" target="_blank">Instagram</a><a href="https://emp3juices.tumblr.com/" target="_blank">Tumblr</a><a href="https://twitter.com/eMp3Juices/" target="_blank">Twitter</a><a href="https://www.yt-mp3s.com/@developers" target="_blank">API</a>
+</div>
+<div id="content">
+
+<a href=""><img id="logo" src="images/logo.png" alt="MP3Juice - Free MP3 Music Downloader" title="MP3Juice - Free MP3 Music Downloader"></a>
+<div class="lr">
+<form action="https://mp3-juice.site/" method="post" id="searchform" class="navbar-frm">
+<div class="input-group col-md-12">
+<span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span>
+<input type="text" class="search-query search form-control input-lg" required="required" id="autoinput" autocomplete="off" title="" name="q" placeholder="Youtube URL or  Type Keyword" />
+<span class="input-group-btn"><button class="btn btn-default btn-lg" type="submit">
+<span class="glyphicon glyphicon-search"></span> </button></span>
+</div>
+</form> </div>
+<div id="form_text">
+By pressing &rdquo;Search&rdquo; you confirm your consent to our <a href="terms-of-use/">Terms of Use</a>.<br>
+</div>
+</center> <div id="content">
+<center>
+<br>
+<h1><strong>MP3Juice</strong></h1>
+<p>Welcome to MP3Juice site for free download music is a known free mp3 search engine for mp3 convert and download is one of the most popular and biggest <strong>free mp3 music</strong> search engine. We make it so simple so type in your search query in search bar and shoot find button and you are done with a list of your results.</p>
+<p>Search for your <strong><a href="https://www.youtube.com/watch?v=3tXXrble3OI">free music download</a></strong> for free with our comfortable search engine. Listen to unlimited mp3 audio files on your Mobile, Tablet, Laptop or Desktop. Enjoy lossless quality during streaming the results on the fly to your device. <strong>MP3Juice</strong> is a worldwide service and available in each country. Our search engine belongs to the biggest and most popular of the web. We are using latest techniques that makes it possible to offer you the whole service without downloading any software or plugin for <strong><a href="https://www.yt-mp3s.com/">youtube to mp3 converter</a></strong>.</p>
+<p>The whole website and functionality is free, you don't need any expensive premium subscription and no high end device.</p>
+
+<p>
+Another cool thing is that you can also paste in a youtube URL/ID and click the find button to find and convert a video into an mp3 HD 3Gp WEBM HQ formats. As soon it is ready you will be able to download the converted file.
+</p>
+
+<div id="form_text">
+<p>The usage of our website is free and does not require any software or registration. By using our website you accept our <a href="terms-of-use/">Terms of Use</a></p>
+<h2>Friends</h2>
+<a href="https://www.musicpleer.club/">Musicpleer</a> | <a href="https://www.yt-mp3s.com/">Youtube API</a> | <a href="https://mp3skulljuice.xyz/">MP3Skulls</a>
+</div>
+
+
+
+
+
+
+</div>
+</div></center>
+<center><div id="footer">
+<a href="contact/">Contact</a><a href="copyright-claims/">Copyright Claims</a><a href="privacy-policy/">Privacy Policy</a><a href="faq/">FAQ</a><a href="terms-of-use/">ToS</a><br>
+
+
+</div></center>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript">var dsz = {"url":""};</script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-159440979-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-159440979-1');
+</script>
+<script>
+    jQuery(document).ready(function($) {
+	$('.search-form input[type=text]').focus();
+	
+	if ( $('.items-list').length ) {
+		$('.items-list').masonry({
+			itemSelector: '.item'
+		});
+	}
+	
+	$('.btn-download').on('click', function() {
+		var data_did = $(this).attr('data-did'), 
+			data_name = $(this).attr('data-name'),
+			data_point = $(this).attr('data-point');
+			
+		$('.downloader').html('');
+		$('.btn-download').show();
+		$('.btn-close').hide();
+			
+		if ( window.XMLHttpRequest ) {
+			xmlhttp = new XMLHttpRequest()
+		} else {
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.open( "GET", dsz.url + "/data/both.php?v=" + data_did + "" );
+		xmlhttp.onreadystatechange = function() {		
+			if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+				$('#downloader-' + data_did).html(xmlhttp.responseText);
+				$(this).text("Stop");			
+			}
+		};
+		
+		xmlhttp.send(null);
+		
+		$(this).hide();
+		$('#item-' + data_did + ' .btn-close').show();
+	});
+	
+	$('.btn-close').on('click', function() {
+		var data_did = $(this).attr('data-did');
+			
+		$('.downloader').html('');
+		$(this).hide();
+		$('.btn-download').show();
+	});
+});
+</script>
+<script>
+jQuery(document).ready(function($) {
+	$('.search-form input[type=text]').focus();
+	
+	if ( $('.items-list').length ) {
+		$('.items-list').masonry({
+			itemSelector: '.item'
+		});
+	}
+	
+	$('.btn-play').on('click', function() {
+		var data_id = $(this).attr('data-id'), 
+			data_title = $(this).attr('data-title'),
+			data_source = $(this).attr('data-source');
+			
+		$('.player').html('');
+		$('.btn-play').show();
+		$('.btn-stop').hide();
+			
+		if ( window.XMLHttpRequest ) {
+			xmlhttp = new XMLHttpRequest()
+		} else {
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.open( "GET", dsz.url + "/data/video-player.php?v=" + data_id + "" );
+		xmlhttp.onreadystatechange = function() {		
+			if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+				$('#player-' + data_id).html(xmlhttp.responseText);
+				$(this).text("Stop");			
+			}
+		};
+		
+		xmlhttp.send(null);
+		
+		$(this).hide();
+		$('#item-' + data_id + ' .btn-stop').show();
+	});
+	
+	$('.btn-stop').on('click', function() {
+		var data_id = $(this).attr('data-id');
+			
+		$('.player').html('');
+		$(this).hide();
+		$('.btn-play').show();
+	});
+});
+
+</script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+$.fn.googleSuggest = function(opts){
+	opts = $.extend({service: 'youtube', secure: false}, opts);
+
+	var services = {
+		youtube: { client: 'youtube', ds: 'yt' },
+		books: { client: 'books', ds: 'bo' },
+		products: { client: 'products-cc', ds: 'sh' },
+		news: { client: 'news-cc', ds: 'n' },
+		images: { client: 'img', ds: 'i' },
+		web: { client: 'psy', ds: '' },
+		recipes: { client: 'psy', ds: 'r' }
+	}, service = services[opts.service];
+		opts.delay = 0;
+	opts.source = function(request, response){
+		$.ajax({
+		url: 'https://clients1.google.com/complete/search',
+		dataType: 'jsonp',
+		data: {
+			q: request.term,
+			nolabels: 't',
+			client: service.client,
+			ds: service.ds
+		},
+		success: function(data) {
+			response($.map(data[1], function(item){
+			return { value: $("<span>").html(item[0]).text() };
+			}));
+		}
+		});
+	};
+	
+	opts.select = function(event,ui){
+		$("#autoinput").val(ui.item.label);
+		$("#autoform").submit();	
+	};
+	
+	return this.each(function(){
+		$(this).autocomplete(opts);
+	});
+	}
+	$('#autoinput').googleSuggest();
+});
+</script>
